7ec81569c92fa5329f179a8c67f35c80d153ece3
ahmadabdullaah0-lgtm <ahmadabdullaah0@gmail.com>
2026-08-19 23:08:29 +0530

    Time: 2 ms (88.65%), Space: 46.3 MB (95.9%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -5,24 +5,35 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
  * }
  */
+
 class Solution {
 
     public ListNode mergeKLists(ListNode[] lists) {
 
-        if (lists.length == 0) {
+        if (lists == null || lists.length == 0) {
             return null;
         }
 
-        ListNode result = lists[0];
+        int interval = 1;
 
-        for (int i = 1; i < lists.length; i++) {
-            result = mergeTwoLists(result, lists[i]);
+        while (interval < lists.length) {
+
+            for (int i = 0; i + interval < lists.length; i += interval * 2) {
+
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+
+            }
+
+            interval *= 2;
         }
 
-        return result;
+        return lists[0];
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
@@ -30,8 +41,8 @@ class Solution {
         ListNode dummy = new ListNode();
         ListNode current = dummy;
 
-        while (list1 != null && list2 != null)
-         {
+        while (list1 != null && list2 != null) {
+
             if (list1.val <= list2.val) {
                 current.next = list1;
                 list1 = list1.next;
