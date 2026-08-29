afdaa22f160323072deba04be664bc8921b0882e
ahmadabdullaah0-lgtm <ahmadabdullaah0@gmail.com>
2026-08-30 01:51:14 +0530

    Time: 0 ms (100%), Space: 46.5 MB (31.68%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,10 +1,17 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        for(char letter : letters)
-        {
-            if (letter>target)
-            return letter;
+        int l = 0, r = letters.length - 1;
+        char ans = letters[0];
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (letters[mid] > target) {
+                ans = letters[mid];
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
-        return letters[0];
+        return ans;
     }
 }
