c1f5c330078935444ead4fec90ac8390e6fdb22d
ahmadabdullaah0-lgtm <ahmadabdullaah0@gmail.com>
2026-09-02 01:32:27 +0530

    Time: 1 ms (100%), Space: 43.6 MB (25.81%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,29 +1,16 @@
-import java.util.Stack;
-import java.util.Collections;
-
 class Solution {
     public int maxPower(String s) {
-
-        int start = 0;
-        int c = 1;
-
-        Stack<Integer> stack = new Stack<>();
-
-        while (start < s.length() - 1) {
-
-            if (s.charAt(start) == s.charAt(start + 1)) {
-                c++;
-            } 
+        int current = 1;
+        int max = 1;
+        for(int i = 1; i < s.length(); i++){
+            char c = s.charAt(i);
+            char last = s.charAt(i-1);
+            if(c == last) current++;
             else {
-                stack.push(c);
-                c = 1;
+                max = Math.max(current, max);
+                current = 1;
             }
-
-            start++;
         }
-
-        stack.push(c);
-
-        return Collections.max(stack);
+        return Math.max(current, max);
     }
 }
